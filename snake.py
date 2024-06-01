75aba11c6098a642f7eafc16397f0acc12e69a2e
Con <ckritikos@gmail.com>
2024-06-01 17:51:49 +1000

    Update code in snake class Use init properly Add constants for values that are expected to be constant

--- snake.py
+++ snake.py
@@ -1,20 +1,26 @@
 from turtle import Turtle
 
-class Snake:
-    # create a list for the snake body
-    snake_body = []
+# CONSTANTS -START ###
+MOVE_DISTANCE = 20
+TURN_ANGLE = 90
+# CONSTANTS -END ###
 
-    # create three Turtle objects
-    # each Turtle should be a white square (default size:20x20)
-    for body_index in range(0, 3):
-        body_part = Turtle(shape="square")
-        body_part.color("white")
-        body_part.penup()
-        body_part.goto(0 - (body_index * 20), 0)
-        snake_body.append(body_part)
+class Snake:
 
     def __init__(self):
-        pass
+        # create a list for the snake body
+        self.snake_body = []
+        self.create_snake()
+
+    def create_snake(self):
+        # create three Turtle objects
+        # each Turtle should be a white square (default size:20x20)
+        for body_index in range(0, 3):
+            body_part = Turtle(shape="square")
+            body_part.color("white")
+            body_part.penup()
+            body_part.goto(0 - (body_index * 20), 0)
+            self.snake_body.append(body_part)
 
     def move_forward(self):
         """
@@ -33,19 +39,19 @@ class Snake:
                 self.snake_body[seg_num].goto(segment_in_front_pos)
             # when the loop cycle reaches the head, the head is just meant to move forward
             else:
-                self.snake_body[seg_num].forward(20)
+                self.snake_body[seg_num].forward(MOVE_DISTANCE)
 
     def turn_left(self):
         """
         Turns the head of the snake body left 90 degrees
         :return:
         """
-        self.snake_body[0].setheading(self.snake_body[0].heading() + 90)
+        self.snake_body[0].setheading(self.snake_body[0].heading() + TURN_ANGLE)
 
     def turn_right(self):
         """
         Turns the head of the snake body right 90 degrees
         :return:
         """
-        self.snake_body[0].setheading(self.snake_body[0].heading() - 90)
+        self.snake_body[0].setheading(self.snake_body[0].heading() - TURN_ANGLE)
 
